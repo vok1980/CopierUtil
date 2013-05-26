663771eb65bfd71602221fb97c183892da8efd37
Vyacheslav O. Koshcheuev <vkoscheev@mail.ru>
2013-05-26 19:33:25 +0700

    misc

---
+++ src/copy.c
@@ -0,0 +1,8 @@
+
+
+
+
+
+void CopyList()
+{
+}
--- src/main.c
+++ src/main.c
@@ -1,3 +1,41 @@
 
 
+#include <stdio.h>
+#include <string.h>
+
+//#include <vector>
+//#include <string>
+
+
+int main(int argc, char *argv[])
+{
+//	std::vector<std::string> aParams;
+
+	//while (--argc > 0)
+	//	printf("%s \n", argv[argc]);
+
+	if (argc > 1)
+	{
+		if ( 0 == strcmp (argv[1] , "-g")  )		
+		{
+			GenerateList();
+		}
+		else if (0 == strcmp (argv[1] , "-c"))
+		{
+			CopyList();
+		}
+		else
+		{
+			PrintHelp();
+		}
+	}
+	else
+	{
+		PrintHelp();
+	}
+
+
+	return 0;
+}
+
 
