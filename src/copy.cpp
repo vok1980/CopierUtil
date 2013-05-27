8bd72cd71b936a141e695258575ec40866ed86e1
Vyacheslav <vkoscheev@mail.ru>
2013-05-27 18:39:02 +0700

    copy impl

--- src/copy.cpp
+++ src/copy.cpp
@@ -2,8 +2,11 @@
 #include <iostream>						// for std::cout
 #include <string>
 #include "boost/filesystem.hpp"			// includes all needed Boost.Filesystem declarations
+//#include "boost/iostreams/device/file.hpp"
+#include "boost/filesystem/fstream.hpp"
 
 using namespace boost::filesystem;      // for ease of tutorial presentation;
+//using namespace boost::iostreams;
 
 
 
@@ -12,6 +15,7 @@ void CopyList(const std::string &strFileList, const std::string &strSrcPath, con
 {
 	path path_src_dir(strSrcPath);
 	path path_dest_dir(strDstPath);
+	path path_list_file(strFileList);
 
 	if ( !exists( path_src_dir ) ) 
 	{
@@ -30,9 +34,35 @@ void CopyList(const std::string &strFileList, const std::string &strSrcPath, con
 		}
 	}
 
+//	wfile fsList(strFileList);
+//	fsList.open(strFileList, BOOST_IOS::in, BOOST_IOS::in);
+
+	boost::filesystem::wifstream fsList(path_list_file);
+	
+	if (!fsList.is_open())
+	{
+		std::cerr << "Failed to read file \"" + strFileList + "\"" << std::endl;
+	}
+
+#define BUF_SIZE	1024
+	wchar_t szBuf[BUF_SIZE];
+	std::list<std::wstring> aList;
+
+	while (fsList.getline(szBuf, BUF_SIZE))
+	{
+		aList.push_back(szBuf);
+	}
 
 
+	for (std::list<std::wstring>::iterator it = aList.begin(); it != aList.end(); ++it)
+	{
+		path src_file( path_src_dir );
+		path dst_file( path_dest_dir );
 
-
-
+		src_file += *it;
+		dst_file += *it;
+		
+		//boost::system::error_code errcode;
+		copy(src_file, dst_file/*, &errcode*/);
+	}
 }
